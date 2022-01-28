a1b8586237689863b71666c36c10d0e7cf081b28
paggimm <bernhardrobl@gmx.de>
2022-01-28 18:58:51 +0100

    add activities table to migration_file

--- lde/postgres/docker-entrypoint-initdb.d/prepare_db.sql
+++ lde/postgres/docker-entrypoint-initdb.d/prepare_db.sql
@@ -3,7 +3,15 @@ create table if not exists users (
 	name varchar(30) unique not null,
 	password varchar(100) not null
 );
+
+create table if not exists activities (
+    id serial primary key,
+    user_id integer references users(id),
+    duration integer,
+    name varchar(30) not null
+);
+
 insert into users(name, password)
 values
 	('bernie', 'AQAAAAEAACcQAAAAEN6JyCrrWIJ7Hs2rZYK+h+ZBOzp3Jrlo9q4QyI4+fqpmeQsUuiTx2AF/WYhdkyS7vA=='),
-	('nico', 'AQAAAAEAACcQAAAAEGdMk6L4pkIgw3HIgfvzmWB2LrUpOHWwIcPZyoVFn4nraoNuoY9DXIp9M4hUeMjrUw==')
+	('nico', 'AQAAAAEAACcQAAAAEGdMk6L4pkIgw3HIgfvzmWB2LrUpOHWwIcPZyoVFn4nraoNuoY9DXIp9M4hUeMjrUw==');
